9a5506dbc1bea886ec9338a89ce1b909232a2a79
Priyadharsini7 <Priyadharsini7>
2023-01-03 18:21:14 +0530

    My WorkSpace

---
+++ src/org/acc/Student.java
@@ -0,0 +1,31 @@
+package org.acc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Student {
+	public static void main(String[] args) {
+		 Set <String> s = new HashSet();
+		       s.add("Lion");
+		       s.add("cat");
+		       s.add("dog");
+		       s.add("Tiger");
+		       System.out.println(s);
+		
+		for (String string : s) {
+			System.out.println(string);
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
